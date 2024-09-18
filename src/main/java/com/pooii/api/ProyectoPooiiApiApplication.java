893ceae8b7e9f4dcebc175e6a5a9e8bd0ed214e1
juanp <juanp@kali>
2024-09-18 01:32:15 -0500

    Laboratorio 2 finalizado
    El objetivo principal de este laboratorio es realizar la configuración
    de seguridad sprint boot y JWT, en donde para cada servicio CRUD ya
    creado requiera doble autenticación a través de la solicitud de token
    previamente generado.

--- src/main/java/com/pooii/api/ProyectoPooiiApiApplication.java
+++ src/main/java/com/pooii/api/ProyectoPooiiApiApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+
 @SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
 public class ProyectoPooiiApiApplication extends SpringBootServletInitializer{
 
